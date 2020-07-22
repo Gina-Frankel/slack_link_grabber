177c5bc6e349eb7857166a41d10ac9c9e8123f34
Gina Frankel <g.frankel22@gmail.com>
2020-07-22 14:01:52 +0100

    Change regex to not take airtable links

--- slack_scraper/lib/data_extractor.rb
+++ slack_scraper/lib/data_extractor.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 def extract_links_from_string(string)
-  regex = /data-stringify-link="https:\S*/
+  regex = /data-stringify-link="https:\/\/(?!air)\S*/
   match = string.scan(regex)
   match
 end
+
+# Text - no links
+# regex = /rich_text_section">(?:\w|\ |!|&|;|'|,|:|\.|\?|)*/
+
+# Links + text
+#  regex = /rich_text_section.*data-stringify-link\S*/
+
+# Links
+# regex = /data-stringify-link\S*/
