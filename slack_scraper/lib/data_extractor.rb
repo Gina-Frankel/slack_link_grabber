61791244de978cf82fda52e6a336414fa2f3aac7
Gina Frankel <g.frankel22@gmail.com>
2020-07-22 14:28:18 +0100

    Change regex to not include unwanted detail

--- slack_scraper/lib/data_extractor.rb
+++ slack_scraper/lib/data_extractor.rb
@@ -1,10 +1,13 @@
 # frozen_string_literal: true
 
 def extract_links_from_string(string)
-  regex = /data-stringify-link="https:\/\/(?!air)\S*/
+  regex = %r{(?<=data-stringify-link=")https:\/\/(?!air)\S*}
   match = string.scan(regex)
   match
 end
+
+# Links without airtable
+# regex = /data-stringify-link="https:\/\/(?!air)\S*/
 
 # Text - no links
 # regex = /rich_text_section">(?:\w|\ |!|&|;|'|,|:|\.|\?|)*/
